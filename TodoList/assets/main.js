260c7326c9304e3d12591acb38ba78381d77c360
William Andrey Silva <46973365+Will5507@users.noreply.github.com>
2023-02-22 21:50:13 -0300

    funcionalidade risco

--- TodoList/assets/main.js
+++ TodoList/assets/main.js
@@ -3,56 +3,31 @@ const firstdiv = document.getElementById('firstdiv')
 let valorInput = 0
 valorInput = inputmain.value
 
-const obj = { 'qualquer': valorInput}
 
 
 
-function funcaoGenerica() {
+function criandoLinhas() {
 
-   // console.log(typeof(valorInput))   = string
-   localStorage.setItem(obj, JSON.stringify(obj))
-
-   let j = localStorage.getItem(obj)
-
-   /*let j = localStorage.getItem(obj)
-   console.log()*/
-   
    var exemplo = document.createElement("div")
    exemplo.innerText += inputmain.value
    exemplo.classList.add('novadiv')
-   exemplo.setAttribute('onclick', 'adicionarTagDel()')
+   exemplo.setAttribute('onclick', 'adicionarRisco(this)')
 
    
    firstdiv.appendChild(exemplo)
    
-  // document.body.appendChild(exemplo)
- 
 
 
 }
 
+//Marca a linha que deseja remover da lista.
+  function adicionarRisco (elemento) {
 
-const adicionarTagDel = function() {
+     elemento.classList.toggle('ClassDelete')
 
-  let novadiv = document.querySelector('.novadiv')
- // let valor = novadiv.innerHTML // armazena valor textual da div pai do elemento del.
-  //let adicionarDel = document.createElement('del')// cria elemento del.
- return novadiv.classList.add('classeDelete')
- 
- //novadiv.appendChild(adicionarDel) 
-
-
-}
-
-
-
+  }
   
- 
+  
 
 
 
-
-
-
-
-// Parar criar o <del> na palavra basta usar um função onclick na caixa de texto, quando clicado o del será ativado pro valor do input da caixa.
