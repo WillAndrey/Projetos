5ccc752f2f723cc9ff78f7d7eb1a35da2ae21f6f
WillAndrey <will_g80@outlook.com>
2025-12-25 21:38:47 -0300

    Refatorando projeto calculadora

--- ProjetoCalculadora/assets/main.js
+++ ProjetoCalculadora/assets/main.js
@@ -1,75 +1,52 @@
-//total
 const visor = document.getElementById('visor')
 const main = document.getElementById('main-calc')
-
-// numerais
 const numerais = document.querySelectorAll('.numerais')
-
-// operadores aritmeticos
 const operadores = document.querySelectorAll('.operadores')
-
-//clear
 const clear = document.getElementById('clear')
-
-//igual
 const igual = document.getElementById('equal');
-
 const dot = document.getElementById('dot')
+let op = []
+let elementoClicado = ""
 
-let firstValue = ''
-let op = ''
+main.addEventListener("click", listenerClick)
+//main.addEventListener("keydown", listenerKeydown)
 
-
-const functionCalc = function(event) {
-
-
-   numerais.forEach(function(elemento) {
-        if(event.target == elemento) {
-            visor.value += elemento.value //funcionou
-        }if (event.target == clear) {
-            visor.value = ''
-        }if(event.target == dot) {
-            visor.value += '.'
-        }
-
-   })
-
-   operadores.forEach(function(elemento){
-    if (event.target == elemento) {
-        if (elemento.value === 'adicao') {
-          op = 'adicao'
-        } else if (elemento.value === 'subtracao') {
-          op = 'subtracao'
-        } else if (elemento.value === 'multiplicacao') {
-          op = 'multiplicacao'
-        } else if (elemento.value === 'divisao') {
-          op = 'divisao'
-        }
-        firstValue = visor.value
-        visor.value = null
-      }
-   })     
-
-   if (event.target == igual && op)  { //verificação caso o item clicado foi '=' e se a algum operador foi utilizado.
-    if (op == 'adicao') {
-     visor.value = Number(firstValue) + Number(visor.value)
-     firstValue = visor.value
-     op = ''
-    } else if(op == 'multiplicacao'){
-     visor.value = Number(firstValue) * Number(visor.value)
-     firstValue = visor.value
-     op = ''
-    } else if(op == 'divisao') {
-     visor.value = Number(firstValue) / Number(visor.value)
-     firstValue = visor.value
-     op = ''
-    }else if(op == 'subtracao') {
-     visor.value = Number(firstValue) - Number(visor.value)
-     firstValue = visor.value
-     op = ''
-    }
-}                 
-
-                        
-}
+ 
+function listenerClick(event){
+ 
+        if(event.target.classList.contains("numerais")) {
+            visor.value += event.target.value
+            elementoClicado += event.target.value
             
+        } 
+        
+        if (event.target.classList.contains('clear')) {
+                elementoClicado = ""
+                op.length = 0
+                visor.value = ''
+        }
+
+        if(event.target.classList.contains('operadores')) {
+           
+          visor.value = ''
+          op.push(elementoClicado)
+          op.push(event.target.value)
+          elementoClicado = ""
+          console.log(op)
+        }
+ 
+        if (event.target.classList.contains('igual')){
+                op.push(elementoClicado)
+                let expressao =  op.join("")
+                const resultado = Function(`return ${expressao}`)()
+                op = [resultado]
+                visor.value = resultado
+        }
+
+}
+
+//function listenerKeydown(event) {}
+
+
+    
+
